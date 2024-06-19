2281dd629f3a334bc7f7b8505eaad42114ea663c
revoly afanasev <rewoly@mail.ru>
2024-06-19 23:53:05 +0900

    first commit

---
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/GatekeeperServerLiteApplication.java
@@ -0,0 +1,13 @@
+package com.alrosa.staa.gatekeeper_server_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeeperServerLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeeperServerLiteApplication.class, args);
+	}
+
+}
---
+++ gatekeeper_server_lite/src/test/java/com/alrosa/staa/gatekeeper_server_lite/GatekeeperServerLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alrosa.staa.gatekeeper_server_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperServerLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
