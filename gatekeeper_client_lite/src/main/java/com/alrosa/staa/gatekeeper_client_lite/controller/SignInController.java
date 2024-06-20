5859c8778948a45e7f85b43b8aab82a1519b6385
unknown <rewoly@mail.ru>
2024-06-20 14:53:40 +0900

    first commit

--- gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/SignInController.java
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/SignInController.java
@@ -1,4 +1,14 @@
 package com.alrosa.staa.gatekeeper_client_lite.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 public class SignInController {
+    @FXML
+    private TextField textFieldLogin = new TextField();
+    @FXML
+    private PasswordField passwordField = new PasswordField();
+    @FXML
+    private Button buttonSignIn = new Button();
 }
