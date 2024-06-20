daa0391ea9478cb575e8d5b8949efc09b43e1a59
revoly afanasev <rewoly@mail.ru>
2024-06-21 00:18:36 +0900

    first commit

---
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/UsersEntity.java
@@ -0,0 +1,33 @@
+package com.alrosa.staa.gatekeeper_server_lite.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UsersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "middle_name")
+    private String middle_name;
+    @Column(name = "last_name")
+    private String last_name;
+    @Column(name = "company")
+    private String company;
+    @Column(name = "organization")
+    private String organization;
+    public UsersEntity() {}
+    public UsersEntity(String first_name, String middle_name, String last_name, String company, String organization) {
+        this.first_name = first_name;
+        this.middle_name = middle_name;
+        this.last_name = last_name;
+        this.company = company;
+        this.organization = organization;
+    }
+}
