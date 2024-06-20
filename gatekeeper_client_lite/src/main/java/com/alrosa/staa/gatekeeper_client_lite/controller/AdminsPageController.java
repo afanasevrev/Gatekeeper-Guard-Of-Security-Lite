f481b0977937ccc36dc5736b4c3bb50605a5e627
unknown <rewoly@mail.ru>
2024-06-20 11:36:58 +0900

    first commit

--- gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/AdminsPageController.java
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/AdminsPageController.java
@@ -1,4 +1,4 @@
-package com.alrosa.staa.gatekeeper_client_lite;
+package com.alrosa.staa.gatekeeper_client_lite.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- gatekeeper_client_lite/src/main/java/module-info.java
+++ gatekeeper_client_lite/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.alrosa.staa.gatekeeper_client_lite {
 
     opens com.alrosa.staa.gatekeeper_client_lite to javafx.fxml;
     exports com.alrosa.staa.gatekeeper_client_lite;
+    exports com.alrosa.staa.gatekeeper_client_lite.controller;
+    opens com.alrosa.staa.gatekeeper_client_lite.controller to javafx.fxml;
 }
