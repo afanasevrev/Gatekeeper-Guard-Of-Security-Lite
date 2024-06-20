d140751c6049c8c59180c6a71c488a133e539a63
revoly afanasev <rewoly@mail.ru>
2024-06-20 23:52:45 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/repository/AdminsRepository.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/repository/AdminsRepository.java
@@ -3,4 +3,4 @@ package com.alrosa.staa.gatekeeper_server_lite.repository;
 import com.alrosa.staa.gatekeeper_server_lite.entity.AdminsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminsRepository extends JpaRepository<AdminsEntity, Long> { }
+public interface AdminsRepository extends JpaRepository<AdminsEntity, Long> {}
