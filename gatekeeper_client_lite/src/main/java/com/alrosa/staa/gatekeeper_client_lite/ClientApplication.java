be5feed4072784242bca974d48ba722db9703978
unknown <rewoly@mail.ru>
2024-06-20 12:00:42 +0900

    first commit

--- gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/ClientApplication.java
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/ClientApplication.java
@@ -4,15 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
 public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Войти");
         stage.setScene(scene);
         stage.show();
     }
---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/SignInController.java
@@ -0,0 +1,4 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller;
+
+public class SignInController {
+}
