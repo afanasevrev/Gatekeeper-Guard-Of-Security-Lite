730da25f3091778c6c556272941b9a7ebd1d8b52
revoly afanasev <rewoly@mail.ru>
2024-06-21 13:19:14 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/AdminsEntity.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/AdminsEntity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 /**
- * Класс для администраторов связи
+ * Класс для администраторов для связи с БД
  */
 @Getter
 @Setter
