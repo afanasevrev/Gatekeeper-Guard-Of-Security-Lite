0476d1a23ea42cb741f5a376070996a50dd58bd3
revoly afanasev <rewoly@mail.ru>
2024-06-23 00:17:08 +0900

    first commit

--- gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/AdminsPageController.java
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/AdminsPageController.java
@@ -1,7 +1,16 @@
 package com.alrosa.staa.gatekeeper_client_lite.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+/**
+ * Контроллер для взаимодействия с формой администратора
+ */
 public class AdminsPageController {
-
+    //Вкладка "Пользователи"
+    @FXML
+    private Tab tabUsers = new Tab();
+    @FXML
+    private TextField textFieldSearchUser = new TextField();
+    
 }
