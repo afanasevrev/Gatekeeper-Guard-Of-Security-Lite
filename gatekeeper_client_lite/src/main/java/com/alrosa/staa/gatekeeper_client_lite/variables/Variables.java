aaaf15c982ca4cfb87f0d040533032457c21f194
revoly afanasev <rewoly@mail.ru>
2024-06-24 18:36:46 +0900

    first commit

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/variables/Variables.java
@@ -0,0 +1,6 @@
+package com.alrosa.staa.gatekeeper_client_lite.variables;
+
+public class Variables {
+    public static String server_ip = "localhost";
+    public static int server_port = 8080;
+}
