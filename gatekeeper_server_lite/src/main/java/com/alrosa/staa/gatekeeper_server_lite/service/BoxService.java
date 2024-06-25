94099f6d67b1c87f4b9530d123d9223b076395ef
revoly afanasev <rewoly@mail.ru>
2024-06-26 00:29:24 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/BoxService.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/BoxService.java
@@ -1,6 +1,8 @@
 package com.alrosa.staa.gatekeeper_server_lite.service;
 
+import java.util.List;
+
 public interface BoxService<T> {
     String createBox(Box<T> entity);
-
+    List<T> readBox();
 }
--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/UsersServiceImpl.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/UsersServiceImpl.java
@@ -3,11 +3,17 @@ package com.alrosa.staa.gatekeeper_server_lite.service;
 import com.alrosa.staa.gatekeeper_server_lite.entity.UsersEntity;
 import com.alrosa.staa.gatekeeper_server_lite.repository.UsersRepository;
 
+import java.util.List;
+
 public class UsersServiceImpl implements BoxService<UsersEntity> {
     private UsersRepository usersRepository;
     @Override
     public String createBox(Box<UsersEntity> entity) {
         usersRepository.save(entity.getEntity());
         return "Пользователь успешно добавлен";
+    }
+    @Override
+    public List<UsersEntity> readBox() {
+        return usersRepository.findAll();
     }
 }
