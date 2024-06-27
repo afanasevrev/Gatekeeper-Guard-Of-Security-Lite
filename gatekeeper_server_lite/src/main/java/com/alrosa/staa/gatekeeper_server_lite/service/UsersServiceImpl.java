eff2f7af03f27640d70a2d3cad0daad1add59291
revoly afanasev <rewoly@mail.ru>
2024-06-27 23:46:25 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/UsersServiceImpl.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/UsersServiceImpl.java
@@ -8,26 +8,51 @@ import java.util.List;
  */
 public class UsersServiceImpl implements BoxService<UsersEntity> {
     private UsersRepository usersRepository;
+    /**
+     * Метод добавляет пользователя в БД
+     * @param entity
+     * @return Статус выполнения операции
+     */
     @Override
     public String createBox(UsersEntity entity) {
         usersRepository.save(entity);
         return "Пользователь успешно добавлен";
     }
+    /**
+     * Метод возвращает массив пользователей из БД
+     * @return List
+     */
     @Override
     public List<UsersEntity> readBox() {
         return usersRepository.findAll();
     }
+    /**
+     * Метод возвращает одного пользователя из БД по id
+     * @param id
+     * @return UsersEntity
+     */
     @Override
     public UsersEntity readT(Long id) {
         UsersEntity user = usersRepository.findById(id).get();
         return user;
     }
+    /**
+     * Метод удаляет из БД пользователя по id
+     * @param id
+     * @return Статус выполнения операции
+     */
     @Override
     public String deleteT(Long id) {
         UsersEntity user = usersRepository.findById(id).get();
         usersRepository.delete(user);
         return "Пользователь с ID = " + id + " удален из базы";
     }
+    /**
+     * Метод обновляет пользователя
+     * @param entity UsersEntity
+     * @param id int
+     * @return Статус выполнения операции
+     */
     @Override
     public String updateT(UsersEntity entity, Long id) {
         UsersEntity user = usersRepository.findById(id).get();
