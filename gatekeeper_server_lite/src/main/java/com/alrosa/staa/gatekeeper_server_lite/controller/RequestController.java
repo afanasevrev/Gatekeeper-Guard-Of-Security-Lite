5d30f9f57376a07d7216c7d2f4e86bcd3ff26054
revoly afanasev <rewoly@mail.ru>
2024-06-30 00:04:10 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/controller/RequestController.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/controller/RequestController.java
@@ -1,5 +1,7 @@
 package com.alrosa.staa.gatekeeper_server_lite.controller;
 
+import com.alrosa.staa.gatekeeper_server_lite.entity.AdminsEntity;
+import com.alrosa.staa.gatekeeper_server_lite.service.AdminsServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RequestController {
     @GetMapping("/")
     private String getInfo() {
+        AdminsServiceImpl adminsService = new AdminsServiceImpl();
+        adminsService.createBox(new AdminsEntity("root", "gatekeeper", "Afanasev", "Revoly", "Valerievich", "alrosa", "staa", "software engineer"));
         return "Система контроля и управления доступом. Lite версия";
     }
 }
