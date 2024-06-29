369577314b694495189f52a17ed8a01c23422fc6
revoly afanasev <rewoly@mail.ru>
2024-06-29 23:21:38 +0900

    first commit

---
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/Inspector.java
@@ -0,0 +1,42 @@
+package com.alrosa.staa.gatekeeper_server_lite.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "inspector")
+public class Inspector {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "middle_name")
+    private String middle_name;
+    @Column(name = "last_name")
+    private String last_name;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "company")
+    private String company;
+    @Column(name = "organization")
+    private String organization;
+    @Column(name = "position")
+    private String position;
+    public Inspector() {}
+    public Inspector(String first_name, String middle_name, String last_name, String login, String password, String company, String organization, String position) {
+        this.first_name = first_name;
+        this.middle_name = middle_name;
+        this.last_name = last_name;
+        this.login = login;
+        this.password = password;
+        this.company = company;
+        this.organization = organization;
+        this.position = position;
+    }
+}
