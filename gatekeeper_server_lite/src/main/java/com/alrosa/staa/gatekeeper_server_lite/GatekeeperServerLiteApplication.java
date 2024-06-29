ac800becff94ee58acc82b8596bd349e39086579
revoly afanasev <rewoly@mail.ru>
2024-06-30 00:09:44 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/GatekeeperServerLiteApplication.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/GatekeeperServerLiteApplication.java
@@ -1,9 +1,18 @@
 package com.alrosa.staa.gatekeeper_server_lite;
 
+import com.alrosa.staa.gatekeeper_server_lite.entity.AdminsEntity;
+import com.alrosa.staa.gatekeeper_server_lite.service.AdminsServiceImpl;
+import com.alrosa.staa.gatekeeper_server_lite.service.BoxService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class GatekeeperServerLiteApplication {
+	@Bean
+	public BoxService<AdminsEntity> adminsService() {
+		return new AdminsServiceImpl();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GatekeeperServerLiteApplication.class, args);
 	}
