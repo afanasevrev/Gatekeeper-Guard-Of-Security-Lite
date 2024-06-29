0a36a40b06712c5967a25578576aafde3cab8d8f
revoly afanasev <rewoly@mail.ru>
2024-06-29 16:06:51 +0900

    first commit

---
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/OperatorsEntity.java
@@ -0,0 +1,13 @@
+package com.alrosa.staa.gatekeeper_server_lite.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "operators")
+public class OperatorsEntity {
+    
+}
