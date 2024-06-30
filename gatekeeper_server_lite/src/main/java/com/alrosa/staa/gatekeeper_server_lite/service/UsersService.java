159417928dbedfed36b55255b44331d8c1ec5a15
revoly afanasev <rewoly@mail.ru>
2024-06-30 21:43:08 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/UsersService.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/UsersService.java
@@ -1,10 +1,7 @@
 package com.alrosa.staa.gatekeeper_server_lite.service;
 
-import com.alrosa.staa.gatekeeper_server_lite.entity.AdminsEntity;
 import com.alrosa.staa.gatekeeper_server_lite.entity.UsersEntity;
-
 import java.util.List;
-
 public interface UsersService {
     String createUser(UsersEntity entity);
     List<UsersEntity> readUsers();
