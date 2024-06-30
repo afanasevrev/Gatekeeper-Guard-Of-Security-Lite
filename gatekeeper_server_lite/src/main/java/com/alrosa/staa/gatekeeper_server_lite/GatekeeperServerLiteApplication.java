d07e28fcc83db428fdacb8b1454702e3da5531d3
revoly afanasev <rewoly@mail.ru>
2024-06-30 14:42:43 +0900

    first commit

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/GatekeeperServerLiteApplication.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/GatekeeperServerLiteApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatekeeperServerLiteApplication {
 	@Bean
-	public BoxService<AdminsEntity> adminsService() {
+	public BoxService adminsService() {
 		return new AdminsServiceImpl();
 	}
 	public static void main(String[] args) {
---
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/BoxServiceImpl.java
@@ -0,0 +1,47 @@
+package com.alrosa.staa.gatekeeper_server_lite.service;
+
+import com.alrosa.staa.gatekeeper_server_lite.entity.AdminsEntity;
+import com.alrosa.staa.gatekeeper_server_lite.entity.OperatorsEntity;
+import com.alrosa.staa.gatekeeper_server_lite.entity.UsersEntity;
+import com.alrosa.staa.gatekeeper_server_lite.repository.AdminsRepository;
+import com.alrosa.staa.gatekeeper_server_lite.repository.OperatorsRepository;
+import com.alrosa.staa.gatekeeper_server_lite.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BoxServiceImpl<T> implements BoxService<T> {
+    @Autowired
+    private AdminsRepository adminsRepository;
+    @Autowired
+    private UsersRepository usersRepository;
+    @Autowired
+    private OperatorsRepository operatorsRepository;
+    @Override
+    public String createBox(T entity) {
+        if (entity instanceof AdminsEntity) {
+            adminsRepository.save((AdminsEntity) entity);
+        } else if (entity instanceof UsersEntity) {
+            usersRepository.save((UsersEntity) entity);
+        } else if (entity instanceof OperatorsEntity) {
+            operatorsRepository.save((OperatorsEntity) entity);
+        }
+        return "Успешно добавлено";
+    }
+    @Override
+    public List<T> readBox() {
+        return null;
+    }
+    @Override
+    public T readT(Long id) {
+        return null;
+    }
+    @Override
+    public String deleteT(Long id) {
+        return null;
+    }
+    @Override
+    public String updateT(T entity, Long id) {
+        return null;
+    }
+}
