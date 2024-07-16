23c82d0eb7c39ab333134109c34ff7576ed0cf21
revoly afanasev <rewoly@mail.ru>
2024-07-16 22:00:52 +0900

    added class LogsData

--- gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/operators_page/OperatorsPageController.java
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/operators_page/OperatorsPageController.java
@@ -1,16 +1,17 @@
 package com.alrosa.staa.gatekeeper_client_lite.controller.operators_page;
 
 import com.alrosa.staa.gatekeeper_client_lite.operators_data.LogsData;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
 public class OperatorsPageController implements Initializable {
     //Вертикальный сплиттер
     @FXML
@@ -19,6 +20,17 @@ public class OperatorsPageController implements Initializable {
     private HBox hbox = new HBox();
     @FXML
     private TableView<LogsData> tableViewLogsData = new TableView<LogsData>();
+    private ObservableList<LogsData> observableListLogsData = FXCollections.<LogsData>observableArrayList();
+    @FXML
+    private TableColumn<LogsData, String> tableColumnDate = new TableColumn<LogsData, String>("Дата");
+    @FXML
+    private TableColumn<LogsData, String> tableColumnController = new TableColumn<LogsData, String>("Контроллер");
+    @FXML
+    private TableColumn<LogsData, String> tableColumnDirection = new TableColumn<LogsData, String>("Направление");
+    @FXML
+    private TableColumn<LogsData, String> tableColumnUser = new TableColumn<LogsData, String>("Пользователь");
+    @FXML
+    private TableColumn<LogsData, String> tableColumnAccess = new TableColumn<LogsData, String>("Доступ");
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Выравниваем вертикальный сплиттер при изменении размера окна оператора
--- gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/operators_data/LogsData.java
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/operators_data/LogsData.java
@@ -1,4 +1,67 @@
 package com.alrosa.staa.gatekeeper_client_lite.operators_data;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+/**
+ * Класс для заполнения таблицы
+ */
 public class LogsData {
+    public StringProperty date;
+    public StringProperty controllerName;
+    public StringProperty direction;
+    public StringProperty user;
+    public StringProperty access;
+    public LogsData() {}
+    public LogsData(String date, String controllerName, String direction, String user, String access) {
+        this.date = new SimpleStringProperty(this, "date", date);
+        this.controllerName = new SimpleStringProperty(this, "controllerName", controllerName);
+        this.direction = new SimpleStringProperty(this, "direction", direction);
+        this.user = new SimpleStringProperty(this, "user", user);
+        this.access = new SimpleStringProperty(this, "access", access);
+    }
+    public String getDate() {
+        return date.get();
+    }
+    public StringProperty dateProperty() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date.set(date);
+    }
+    public String getControllerName() {
+        return controllerName.get();
+    }
+    public StringProperty controllerNameProperty() {
+        return controllerName;
+    }
+    public void setControllerName(String controllerName) {
+        this.controllerName.set(controllerName);
+    }
+    public String getDirection() {
+        return direction.get();
+    }
+    public StringProperty directionProperty() {
+        return direction;
+    }
+    public void setDirection(String direction) {
+        this.direction.set(direction);
+    }
+    public String getUser() {
+        return user.get();
+    }
+    public StringProperty userProperty() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user.set(user);
+    }
+    public String getAccess() {
+        return access.get();
+    }
+    public StringProperty accessProperty() {
+        return access;
+    }
+    public void setAccess(String access) {
+        this.access.set(access);
+    }
 }
