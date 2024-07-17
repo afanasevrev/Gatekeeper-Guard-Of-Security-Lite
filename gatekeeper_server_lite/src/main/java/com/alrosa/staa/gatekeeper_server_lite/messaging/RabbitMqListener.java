5f6ba61fef3e7bfd87b39064a40c494931dca1fa
unknown <rewoly@mail.ru>
2024-07-17 14:11:43 +0900

    added config rabbit mq

---
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/messaging/RabbitMqListener.java
@@ -0,0 +1,18 @@
+package com.alrosa.staa.gatekeeper_server_lite.messaging;
+
+import com.google.gson.Gson;
+import org.apache.log4j.Logger;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+public class RabbitMqListener {
+    private Logger logger = Logger.getLogger(RabbitMqListener.class);
+    private Gson gson = new Gson();
+    private String text;
+    @Autowired
+    private AmqpTemplate template;
+    @RabbitListener(queues = "OPERATOR")
+    private void Queue(String message) {
+        
+    }
+}
