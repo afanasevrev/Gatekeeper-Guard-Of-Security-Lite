d7f7d3157b9d2818742493bce41ceed87569f4eb
revoly afanasev <rewoly@mail.ru>
2024-07-19 20:59:50 +0900

    working on controllers

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/AddCardController.java
@@ -0,0 +1,16 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddCardController {
+    @FXML
+    private TextField textFieldIdentifier = new TextField();
+    @FXML
+    private Button buttonAdd = new Button();
+    @FXML
+    private void setButtonAdd() {
+
+    }
+}
