a73253cf4c68998d5b0773c755a69b4e353f2049
revoly afanasev <rewoly@mail.ru>
2024-07-20 18:35:25 +0900

    working on controllers

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/AddControllerController.java
@@ -0,0 +1,18 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddControllerController {
+    @FXML
+    private TextField texFieldControllerName = new TextField();
+    @FXML
+    private TextField textFieldIpAddress = new TextField();
+    @FXML
+    private Button buttonAdd = new Button();
+    @FXML
+    private void setButtonAdd() {
+
+    }
+}
