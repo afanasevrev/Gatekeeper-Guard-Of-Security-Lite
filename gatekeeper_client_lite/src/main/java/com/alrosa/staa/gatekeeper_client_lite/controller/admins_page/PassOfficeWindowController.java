2f7541b77a00274487cd6341216001fd5038def3
revoly afanasev <rewoly@mail.ru>
2024-07-21 22:55:53 +0900

    working on controllers

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/PassOfficeWindowController.java
@@ -0,0 +1,25 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class PassOfficeWindowController {
+    @FXML
+    private TextField textFieldFirstName = new TextField();
+    @FXML
+    private TextField textFieldMiddleName = new TextField();
+    @FXML
+    private TextField textFieldLastName = new TextField();
+    @FXML
+    private TextField textFieldLogin = new TextField();
+    @FXML
+    private PasswordField passwordFieldPassword = new PasswordField();
+    @FXML
+    private Button buttonEdit = new Button();
+    @FXML
+    private void setButtonEdit() {
+
+    }
+}
