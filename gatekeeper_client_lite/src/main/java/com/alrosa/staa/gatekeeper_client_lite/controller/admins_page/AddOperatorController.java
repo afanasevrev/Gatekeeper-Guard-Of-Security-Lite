c4995fb68e56a6e7b973e3712291bd078a1402c6
revoly afanasev <rewoly@mail.ru>
2024-07-21 19:27:53 +0900

    working on controllers

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/AddOperatorController.java
@@ -0,0 +1,25 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AddOperatorController {
+    @FXML
+    private TextField textFieldFirstName = new TextField();
+    @FXML
+    private TextField textFieldMiddleName = new TextField();
+    @FXML
+    private TextField textFieldLastName = new TextField();
+    @FXML
+    private TextField textFieldLogin = new TextField();
+    @FXML
+    private PasswordField passwordFieldPassword = new PasswordField();
+    @FXML
+    private Button buttonAdd = new Button();
+    @FXML
+    private void setButtonAdd() {
+
+    }
+}
