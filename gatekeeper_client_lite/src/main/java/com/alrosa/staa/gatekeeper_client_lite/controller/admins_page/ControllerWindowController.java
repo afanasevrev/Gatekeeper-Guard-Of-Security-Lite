85288d31a3a9710041da030b2e3c3eda2ebf2bbb
revoly afanasev <rewoly@mail.ru>
2024-07-21 21:06:23 +0900

    working on controllers

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/ControllerWindowController.java
@@ -0,0 +1,18 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControllerWindowController {
+    @FXML
+    private TextField textFieldControllerName = new TextField();
+    @FXML
+    private TextField textFieldIpAddress = new TextField();
+    @FXML
+    private Button buttonEdit = new Button();
+    @FXML
+    private void setButtonEdit() {
+
+    }
+}
