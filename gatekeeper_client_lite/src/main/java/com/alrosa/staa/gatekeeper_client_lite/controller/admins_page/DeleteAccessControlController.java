68360d8cde32d5d442cd0be5e090fcdbfa37a1ee
revoly afanasev <rewoly@mail.ru>
2024-07-21 21:31:19 +0900

    working on controllers

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/DeleteAccessControlController.java
@@ -0,0 +1,19 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DeleteAccessControlController {
+    @FXML
+    private Button buttonDelete = new Button();
+    @FXML
+    private Button buttonCancel = new Button();
+    @FXML
+    private void setButtonDelete() {
+
+    }
+    @FXML
+    private void setButtonCancel() {
+
+    }
+}
