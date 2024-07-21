0f1a260a045ebb50e67d20d716d0b1f3a9730e77
revoly afanasev <rewoly@mail.ru>
2024-07-21 20:56:43 +0900

    working on controllers

---
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/CardWindowController.java
@@ -0,0 +1,20 @@
+package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CardWindowController {
+    @FXML
+    private TextField textFieldIdentifier = new TextField();
+    @FXML
+    private TextField textFieldUser = new TextField();
+    @FXML
+    private TextField textFieldAccessControl = new TextField();
+    @FXML
+    private Button buttonClose = new Button();
+    @FXML
+    private void setButtonClose() {
+
+    }
+}
