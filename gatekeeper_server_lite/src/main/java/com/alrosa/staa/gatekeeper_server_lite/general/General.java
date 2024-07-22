3b21a73a78d1e1ab6e52da6e3709c40b2d96f593
unknown <rewoly@mail.ru>
2024-07-22 17:10:09 +0900

    Working on Database

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/general/General.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/general/General.java
@@ -5,9 +5,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class General {
+    private MessageType messageType;
     private String card_identifier;
     public General() {}
-    public General(String card_identifier) {
+    public General(String card_identifier, MessageType messageType) {
         this.card_identifier = card_identifier;
+        this.messageType = messageType;
     }
 }
