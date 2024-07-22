a0aac6ef543041434dad2731b2ec0f249de70a1e
unknown <rewoly@mail.ru>
2024-07-22 17:02:32 +0900

    Working on Database

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/PhotosEntity.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/PhotosEntity.java
@@ -1,4 +1,20 @@
 package com.alrosa.staa.gatekeeper_server_lite.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "photos")
 public class PhotosEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long ig;
+    @Lob
+    @Column(name = "photo")
+    private byte[] photo;
+
 }
