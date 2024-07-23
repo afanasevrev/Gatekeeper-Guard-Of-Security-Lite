9c196ee74f70b2e42a974c40d8cde51f5c78152b
unknown <rewoly@mail.ru>
2024-07-23 17:15:03 +0900

    Работа с БД

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/CardsServiceImpl.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/service/CardsServiceImpl.java
@@ -1,10 +1,33 @@
 package com.alrosa.staa.gatekeeper_server_lite.service;
 
 import com.alrosa.staa.gatekeeper_server_lite.entity.CardsEntity;
+import com.alrosa.staa.gatekeeper_server_lite.entity.UsersEntity;
 import com.alrosa.staa.gatekeeper_server_lite.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-public class CardsServiceImpl {
+
+import java.util.List;
+
+public class CardsServiceImpl implements CardsService {
     @Autowired
     private CardsRepository cardsRepository;
-    
+    @Override
+    public String createCard(CardsEntity entity) {
+        return null;
+    }
+    @Override
+    public List<CardsEntity> readCards() {
+        return null;
+    }
+    @Override
+    public CardsEntity readCard(Long id) {
+        return null;
+    }
+    @Override
+    public String deleteCard(Long id) {
+        return null;
+    }
+    @Override
+    public String updateCard(UsersEntity entity, Long id) {
+        return null;
+    }
 }
