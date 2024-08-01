48147d360d9a041c640230762a4ddc3308911000
revoly afanasev <rewoly@mail.ru>
2024-08-01 22:13:58 +0900

    Работа с @RestController

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/controller/ApplicationController.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/controller/ApplicationController.java
@@ -5,7 +5,6 @@ import com.alrosa.staa.gatekeeper_server_lite.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
