34422ecf260d98fc05101029f04070728b6f388f
revoly afanasev <rewoly@mail.ru>
2024-08-04 15:43:18 +0900

    Работа с БД

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/repository/PhotosRepository.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/repository/PhotosRepository.java
@@ -1,7 +1,10 @@
 package com.alrosa.staa.gatekeeper_server_lite.repository;
 
 import com.alrosa.staa.gatekeeper_server_lite.entity.PhotosEntity;
+import com.alrosa.staa.gatekeeper_server_lite.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface PhotosRepository extends JpaRepository<PhotosEntity, Long> {}
+public interface PhotosRepository extends JpaRepository<PhotosEntity, Long> {
+        PhotosEntity findByUsersEntity(UsersEntity usersEntity);
+}
