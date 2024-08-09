c2e7518a797be8278c0a9c9d1b6d269615c9137e
unknown <rewoly@mail.ru>
2024-08-09 15:40:12 +0900

    Работа с БД

--- gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/AccessLevelsControllersEntity.java
+++ gatekeeper_server_lite/src/main/java/com/alrosa/staa/gatekeeper_server_lite/entity/AccessLevelsControllersEntity.java
@@ -1,9 +1,6 @@
 package com.alrosa.staa.gatekeeper_server_lite.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
