3ab2d8e58b9582524bf44e396a1ebc7492bd7481
unknown <rewoly@mail.ru>
2024-08-12 16:54:11 +0900

    Работа с приложением - Админская консоль

--- gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/AccessControlWindowController.java
+++ gatekeeper_client_lite/src/main/java/com/alrosa/staa/gatekeeper_client_lite/controller/admins_page/AccessControlWindowController.java
@@ -1,16 +1,27 @@
 package com.alrosa.staa.gatekeeper_client_lite.controller.admins_page;
 
+import com.alrosa.staa.gatekeeper_client_lite.response_data.AccessLevels;
 import com.alrosa.staa.gatekeeper_client_lite.variables.Variables;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 public class AccessControlWindowController implements Initializable {
+    //Создаем экземпляр класса RestTemplate
+    private RestTemplate restTemplate = new RestTemplate();
     //Создаем url для соединения с сервером
-    private String getAccessLevel = "http://" + Variables.server_ip + ":" + Variables.server_port + "/getAccessLevel/" + AdminsPageController.valueOfAccessLevel;
+    private String url_GetAccessLevel = "http://" + Variables.server_ip + ":" + Variables.server_port + "/getAccessLevel/" + AdminsPageController.valueOfAccessLevel;
+    private Logger logger = Logger.getLogger(AccessControlWindowController.class);
+    //Создаем экземпляр класса AccessLevels
+    private AccessLevels accessLevel;
     @FXML
     private TextField textFieldAccessControl = new TextField();
     @FXML
@@ -29,7 +40,13 @@ public class AccessControlWindowController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Чистим все поля перед открытием формы
         clearFields();
-
+        ResponseEntity<AccessLevels> response = null;
+        try {
+            response = restTemplate.exchange(url_GetAccessLevel, HttpMethod.GET, null, AccessLevels.class);
+            
+        } catch (RuntimeException e) {
+            logger.error(e);
+        }
     }
     /**
      * Метод позволяет чистить все поля перед открытием формы уровня доступа
